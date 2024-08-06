d034f69e8be4355af2dea251c4da03c585571e5c
nicolas71488 <159440979+nicolas71488@users.noreply.github.com>
2024-08-06 11:39:40 -0300

    Add files via upload

---
+++ desafio13.js
@@ -0,0 +1,24 @@
+var jogarhtml = window.document.getElementById('saida')
+
+function clique1() {
+    jogarhtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 1 </p>'
+}
+function clique2() {
+    jogarhtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 2 </p>'
+}
+function clique3() {
+    jogarhtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 3 </p>'
+}
+
+function parimpar() {
+    let num = Number(window.prompt('digite um número: '))
+    let tipo
+    if (num % 2 == 0) {
+        tipo = '<strong>PAR</satrong>'
+    } else {
+        tipo = '<strong>ÍMPAR</strong>'
+    }
+
+    let res = document.querySelector('section#result')
+    res.innerHTML = `<p>O número digitado foi ${num} e ele é: ${tipo}!</p>`
+}
