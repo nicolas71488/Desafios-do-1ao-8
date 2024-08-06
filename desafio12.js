ba555206bf79f940576ad160ee83553d6b82b155
nicolas71488 <159440979+nicolas71488@users.noreply.github.com>
2024-08-06 08:20:46 -0300

    Add files via upload

---
+++ desafio12.js
@@ -0,0 +1,12 @@
+var jogarhtml = window.document.getElementById('saida')
+
+function clique1() {
+    jogarhtml.innerHTML += '<p> Você Clicou escolheu a priemira opção = botão 1 </p>'
+}
+
+function clique2() {
+    jogarhtml.innerHTML += '<p> Você Clicou escolheu a priemira opção = botão 2 </p>'
+}
+function clique3() {
+    jogarhtml.innerHTML += '<p> Você Clicou escolheu a priemira opção = botão 3 </p>'
+}
