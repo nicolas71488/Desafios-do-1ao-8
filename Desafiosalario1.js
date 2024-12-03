ab59f2aef182e1f38b9b012637be3c7b13277230
nicolas71488 <159440979+nicolas71488@users.noreply.github.com>
2024-12-03 08:59:07 -0300

    Update Desafiosalario1.js

--- Desafiosalario1.js
+++ Desafiosalario1.js
@@ -1,13 +1,8 @@
-const form = document.getElementById('form-pagamento');
-const resultado = document.getElementById('resultado');
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
+function calcular() {
     const nome = document.getElementById('nome').value;
     const salarioBruto = parseFloat(document.getElementById('salario').value);
-
     let imposto = 0;
+
     if (salarioBruto > 5000) {
         imposto = salarioBruto * 0.175;
     } else if (salarioBruto > 4000) {
@@ -20,11 +15,10 @@ form.addEventListener('submit', (event) => {
 
     const salarioLiquido = salarioBruto - imposto;
 
-    resultado.innerHTML = `
-        <h2>Resultado</h2>
+    document.getElementById('resultado').innerHTML = `
         <p>Nome: ${nome}</p>
         <p>Salário Bruto: R$ ${salarioBruto.toFixed(2)}</p>
         <p>Imposto de Renda: R$ ${imposto.toFixed(2)}</p>
         <p>Salário Líquido: R$ ${salarioLiquido.toFixed(2)}</p>
     `;
-});
+}
